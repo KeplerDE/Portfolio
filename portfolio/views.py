7d0305e2667f3a2503c3b961c144e945e2a28a25
Dennny <keplerde@gmail.com>
2022-07-21 17:18:56 +0200

    test

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,8 +4,12 @@ from .models import Project
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects':projects})
+
+    return render(request, 'portfolio/home.html', {'projects': projects})
 
 def profile(request):
     return render(request, 'portfolio/profile.html')
 
+def prodetail(request):
+    project = Project.objects.all()
+    return render(request, 'portfolio/prodetail.html', {'project'})
